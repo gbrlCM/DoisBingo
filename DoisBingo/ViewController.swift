b3711a5921abdddaf8ed8631b6fb1aa637f9ce5f
gbrlcm <gabrielfcarvalho360@icloud.com>
2023-01-25 14:26:17 -0300

    inital commit

---
+++ DoisBingo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoisBingo
+//
+//  Created by Gabriel Ferreira de Carvalho on 25/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
